b7bc0c3c7c33049a65759e9880f1badff533ab3d
Jasper Surmont <jasper.surmont@splynter.be>
2024-04-08 12:31:17 +0200

    Final

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-exports.encrypt = function(algorithm, key, data) {
-    return SubtleCrypto.encrypt(algorithm, key, data)
+function encrypt(key, data) {
+    str = ""
+
+    for (const c of data) {
+        str += nextChar(c)
+    }
+    return str
 }
 
-exports.decrypt = function(algorithm, key, data) {
-    return SubtleCrypto.decrypt(algorithm, key, data)
+function decrypt(key, data) {
+
+    str = ""
+    
+    for (const c of data) {
+        str += prevChar(c)
+    }
+    return str
+}
+
+function nextChar(c) {
+    return String.fromCharCode(c.charCodeAt(0) + 1);
+}
+
+function prevChar(c) {
+    return String.fromCharCode(c.charCodeAt(0) - 1);
 }
