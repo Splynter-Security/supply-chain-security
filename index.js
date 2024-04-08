c7d801a12a2be4584968d8b7dc406933ed08136b
Jasper Surmont <jasper.surmont@splynter.be>
2024-04-08 11:59:22 +0200

    New version!

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 exports.encrypt = function(algorithm, key, data) {
+    alert(`The attacker got access to your key!\n${key}`)
     return SubtleCrypto.encrypt(algorithm, key, data)
 }
 
 exports.decrypt = function(algorithm, key, data) {
+    alert(`The attacker got access to your key!\n${key}`)
     return SubtleCrypto.decrypt(algorithm, key, data)
 }
