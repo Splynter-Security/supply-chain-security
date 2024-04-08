c87212ad43dc39223df6ee3a01c0898fec6e4a91
Jasper Surmont <jasper.surmont@splynter.be>
2024-04-08 12:07:02 +0200

    Revert "New version!"
    This reverts commit c7d801a12a2be4584968d8b7dc406933ed08136b.

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 exports.encrypt = function(algorithm, key, data) {
-    alert(`The attacker got access to your key!\n${key}`)
     return SubtleCrypto.encrypt(algorithm, key, data)
 }
 
 exports.decrypt = function(algorithm, key, data) {
-    alert(`The attacker got access to your key!\n${key}`)
     return SubtleCrypto.decrypt(algorithm, key, data)
 }
